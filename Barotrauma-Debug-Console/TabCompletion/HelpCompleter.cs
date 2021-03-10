2b5fb0a700c04699935dcf18db685c837f9ddb16
Jlobblet <jlobblet-github@gmx.co.uk>
2021-03-10 22:29:52 +0000

    Add aliases to HelpCompleter.cs

--- Barotrauma-Debug-Console/TabCompletion/HelpCompleter.cs
+++ Barotrauma-Debug-Console/TabCompletion/HelpCompleter.cs
@@ -6,10 +6,12 @@ namespace Barotrauma_Debug_Console.TabCompletion
     public class HelpCompleter : ICompleter
     {
         private readonly string[] commands = Program.Handler.Commands.Select(c => c.Name).ToArray();
+        private readonly string[] aliases = Program.Handler.Commands.SelectMany(c => c.Aliases).ToArray();
 
         public bool TryComplete(string input, out string output)
         {
-            output = commands.FirstOrDefault(i => i.StartsWith(input, StringComparison.OrdinalIgnoreCase));
+            output = commands.FirstOrDefault(i => i.StartsWith(input, StringComparison.OrdinalIgnoreCase)) ??
+                     aliases.FirstOrDefault(i => i.StartsWith(input, StringComparison.OrdinalIgnoreCase));
             return output is not null;
         }
     }
